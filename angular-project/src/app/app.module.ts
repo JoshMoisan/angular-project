e8b7504c9503ed856d1e50c212c588ac4c4af3f5
joshmoisan <josh.moisan@hotmail.fr>
2023-12-20 19:17:10 -0500

    new component

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceComponent } from './service/service.component';
+import { IntroComponent } from './intro/intro.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServiceComponent
+    ServiceComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule,
